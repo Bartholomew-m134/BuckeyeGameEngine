d97b00c0fada40cf89a7d30f9a2f907681b904da
Matt Bartholomew <Bartholomew.99@osu.edu>
2015-09-21 09:34:35 -0400

    Created Concrete Controllers

---
+++ Game/Game/Controllers/GamePadController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Controllers
+{
+    public class GamePadController : IController
+    {
+        private Dictionary<Buttons, ICommand> buttonMappings;
+
+        public GamePadController(Game1 Game)
+        {
+            buttonMappings = new Dictionary<Buttons, ICommand>();
+        }
+
+        public void RegisterCommand(Buttons button, ICommand command)
+        {
+            buttonMappings.Add(button, command);
+        }
+
+        public void Update()
+        {
+            GamePadState currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.Buttons.A == ButtonState.Pressed)
+            {
+                buttonMappings[Buttons.A].Execute();
+            }
+            if (currentState.Buttons.B == ButtonState.Pressed)
+            {
+                buttonMappings[Buttons.B].Execute();
+            }
+            if (currentState.Buttons.X == ButtonState.Pressed)
+            {
+                buttonMappings[Buttons.X].Execute();
+            }
+            if (currentState.Buttons.Start == ButtonState.Pressed)
+            {
+                buttonMappings[Buttons.Start].Execute();
+            } 
+        }
+    }
+}
---
+++ Game/Game/Controllers/KeyboardController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+    public class KeyboardController : IController
+    {
+        private Dictionary<Keys, ICommand> keyMappings;
+
+        public KeyboardController(Game1 Game)
+        {
+            keyMappings = new Dictionary<Keys, ICommand>();
+            
+        }
+
+        public void RegisterCommand(Keys key, ICommand command)
+        {
+            keyMappings.Add(key, command);
+        }
+
+        public void Update()
+        {
+            Keys[] pressedKeys = Keyboard.GetState().GetPressedKeys();
+
+            foreach (Keys key in pressedKeys)
+            {
+                if (keyMappings.ContainsKey(key))
+                {
+                    keyMappings[key].Execute();
+                }
+            }
+        }
+    }
+}
