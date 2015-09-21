11a7edbaa601cd5b8a5235093f3d660ef5681c0d
Matt Bartholomew <Bartholomew.99@osu.edu>
2015-09-21 19:54:27 -0400

    Small changes.

--- Game/Game/SpriteFactories/BlockSpriteFactory.cs
+++ Game/Game/SpriteFactories/BlockSpriteFactory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,15 @@ namespace Game.SpriteFactories
     public static class BlockSpriteFactory
     {
         private static Texture2D blockSpriteSheet;
+
+        public static void Load(ContentManager content, GraphicsDevice device)
+        {
+
+        }
+
+        public static void Unload()
+        {
+
+        }
     }
 }
