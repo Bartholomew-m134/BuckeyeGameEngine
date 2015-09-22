472645026ec1a2528e93834c897869068c118b89
John Cramer <cramer.240@buckeyemail.osu.edu>
2015-09-22 19:33:14 -0400

    Reupload Enemy Design Pattern

---
+++ Game/Game/Enemies/GoombaClasses/GoombaStates/GoombaFlippedState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Enemies.GoombaClasses.GoombaStates
+{
+    interface GoombaFlippedState : IGoombaState
+    {
+    }
+}
---
+++ Game/Game/Enemies/GreenKoopaClasses/GreenKoopaStates/GreenKoopaHidingInShellState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Enemies.GreenKoopaClasses.GreenKoopaStates
+{
+    class GreenKoopaHidingInShellState : IGreenKoopaState
+    {
+    }
+}
