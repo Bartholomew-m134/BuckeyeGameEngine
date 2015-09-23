0a0f6fb554e2e67e0fbd03e3078d6a53de71989b
Connor Swick <swick.64@osu.edu>
2015-09-23 16:30:31 -0400

    further implemented mario sprites

--- Game/Game/Mario/MarioSprites/FireLeftRunningSprite.cs
+++ Game/Game/Mario/MarioSprites/FireLeftRunningSprite.cs
@@ -15,6 +15,9 @@ namespace Game.Mario.MarioSprites
         private Texture2D spriteSheet;
         private ArrayList fireLeftRunningSpriteDimensions;
         private ArrayList fireLeftRunningSpriteLocations;
+        private int currentSprite;
+        Vector2 currentDimensions;
+        Vector2 currentLocation;
 
         Vector2 firstFireLeftRunningSpriteLocation;
         Vector2 secondFireLeftRunningSpriteLocation;
@@ -26,20 +29,38 @@ namespace Game.Mario.MarioSprites
         Vector2 thirdFireLeftRunningSpriteDimensions;
         Vector2 fourthFireLeftRunningSpriteDimensions;
 
+        Rectangle sourceRectangle;
+        Rectangle destinationRectangle;
+
         public FireLeftRunningSprite(Texture2D spriteSheet, Game1 game)
         {
             this.game = game;
             this.spriteSheet = spriteSheet;
-            //width, height to fill in
-            firstFireLeftRunningSpriteDimensions = ();
-            secondFireLeftRunningSpriteDimensions = ();
-            thirdFireLeftRunningSpriteDimensions = ();
-            fourthFireLeftRunningSpriteDimensions = ();
-            //x,y locations on sheet to fill in
-            firstFireLeftRunningSpriteLocation = ();
-            secondFireLeftRunningSpriteLocation = ();
-            thirdFireLeftRunningSpriteLocation = ();
-            fourthFireLeftRunningSpriteLocation = ();
+            currentSprite = 0;
+            
+            firstFireLeftRunningSpriteDimensions.X = 15;
+            firstFireLeftRunningSpriteDimensions.Y = 31;
+
+            secondFireLeftRunningSpriteDimensions.X = 13;
+            secondFireLeftRunningSpriteDimensions.Y = 30;
+
+            thirdFireLeftRunningSpriteDimensions.X = 15;
+            thirdFireLeftRunningSpriteDimensions.Y = 29;
+
+            fourthFireLeftRunningSpriteDimensions.X = 15;
+            fourthFireLeftRunningSpriteDimensions.Y = 29;
+ 
+            firstFireLeftRunningSpriteLocation.X = 152;
+            firstFireLeftRunningSpriteLocation.Y = 153;
+
+            secondFireLeftRunningSpriteLocation.X = 128;
+            secondFireLeftRunningSpriteLocation.X = 152;
+
+            thirdFireLeftRunningSpriteLocation.X = 102;
+            thirdFireLeftRunningSpriteLocation.Y = 152;
+
+            fourthFireLeftRunningSpriteLocation.X = 77;
+            fourthFireLeftRunningSpriteLocation.Y = 152;
             
             fireLeftRunningSpriteDimensions.Add(firstFireLeftRunningSpriteDimensions);
             fireLeftRunningSpriteDimensions.Add(secondFireLeftRunningSpriteDimensions);
@@ -53,13 +74,18 @@ namespace Game.Mario.MarioSprites
         }
         void Update()
         {
-
+            if (currentSprite < 4)
+            {
+                currentDimensions=  (Vector2)fireLeftRunningSpriteDimensions[currentSprite];
+                currentLocation = (Vector2)fireLeftRunningSpriteLocations[currentSprite];
+                currentSprite++;
+            }
         }
 
         void Draw(SpriteBatch spriteBatch, Vector2 location)
         {
-            Rectangle sourceRectangle = new Rectangle(sheetXLocation, sheetYLocation, width, height);
-            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, height);
+            sourceRectangle = new Rectangle(sheetXLocation, sheetYLocation, width, height);
+            destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, height);
 
             spriteBatch.Begin();
             spriteBatch.Draw(spriteSheet, destinationRectangle, sourceRectangle, Color.White);
