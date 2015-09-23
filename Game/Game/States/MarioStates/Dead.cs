93da66f0e3b393f9be0dc91986ad5fc7b85d23c8
Jackson Luken <jacksonluken@yahoo.com>
2015-09-23 17:34:30 -0400

    fixed Dead State

--- Game/Game/States/MarioStates/Dead.cs
+++ Game/Game/States/MarioStates/Dead.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Game.Mario;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework;
+
 
 namespace Game.States
 {
@@ -13,13 +11,12 @@ namespace Game.States
     {
 
         private MarioInstance mario;
-        private Game1 game;
-        private Texture2D spriteSheet;
+        Game1 game;
 
         public Dead(MarioInstance mario)
         {
             this.mario = mario;
-            mario.sprite = new Mario.MarioSprites.DeadSprite(spriteSheet, game);
+            SpriteFactories.MarioSpriteFactory.CreateDeadSprite(game);
         }
 
 
