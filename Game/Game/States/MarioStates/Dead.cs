a8d77a02c6cee22498b200eab79a42459e543e71
Jackson Luken <jacksonluken@yahoo.com>
2015-09-23 12:23:11 -0400

    Added sprite instance to Mario class and began adding sprites to state constructors

---
+++ Game/Game/States/MarioStates/Dead.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Game.Mario;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Game.States.MarioStates
+{
+    class Dead : IMarioState
+    {
+
+        private MarioInstance mario;
+        private Game1 game;
+        private Texture2D texture;
+
+        public Dead(MarioInstance mario)
+        {
+            this.mario = mario;
+            mario.sprite = new Game.Mario.MarioSprites.DeadSprite(texture, game);
+
+        }
+    
+    }
+}
