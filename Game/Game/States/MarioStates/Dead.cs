d9baf517139eef3e21f6990ab781219fe274349a
Connor Swick <swick.64@osu.edu>
2015-09-23 17:23:33 -0400

    running sprite implementation

---
+++ Game/Game/States/MarioStates/Dead.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Game.Mario;
+
+namespace Game.States
+{
+    class Dead : IMarioState
+    {
+                private MarioInstance mario;
+
+        public Dead(MarioInstance mario)
+        {
+            this.mario = mario;
+        }
+
+    }
+}
