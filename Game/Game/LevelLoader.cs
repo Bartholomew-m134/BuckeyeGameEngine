e7897dd1dbae239053f2fe40628abdd94bdeb6db
Matt Bartholomew <Bartholomew.99@osu.edu>
2015-10-06 19:57:24 -0400

    Added WorldManager and LevelLoader

---
+++ Game/Game/LevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using Game.Blocks;
+using Game.Enemies.GoombaClasses;
+using Game.Enemies.GreenKoopaClasses;
+using Game.Items;
+using Game.Mario;
+using Game.Pipes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+    public class LevelLoader
+    {
+        public LevelLoader()
+        {
+
+        }
+
+        public void Load(string filename, List<IMario> players, List<IBlock> blocks, List<IPipe> pipes, List<Goomba> goombas, List<GreenKoopa> koopas, List<IItem> items)
+        {
+
+        }
+
+    }
+}
