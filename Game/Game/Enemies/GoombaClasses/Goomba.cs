dd211f82fdf1ce791a2ba7976999e37ee44298c4
John Cramer <cramer.240@buckeyemail.osu.edu>
2015-10-06 20:36:57 -0400

    Added some collision stuff.

--- Game/Game/Enemies/GoombaClasses/Goomba.cs
+++ Game/Game/Enemies/GoombaClasses/Goomba.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework;
 
 namespace Game.Enemies.GoombaClasses
 {
-    public class Goomba
+    public class Goomba : IGoomba
     {
         public IGoombaState state;
         public ISprite goombaSprite;
---
+++ Game/Game/Enemies/GoombaClasses/IGoomba.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Enemies.GoombaClasses
+{
+    public interface IGoomba
+    {
+        public void Draw();
+        public void Update();
+    }
+}
