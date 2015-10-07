14eeedb7ef491204165479cf5c0c59cdca34bc19
Kyle Powers <powerskyle26@yahoo.com>
2015-10-07 19:05:55 -0400

    Added collsion data class, added constructor for MarioItemCollisionHandler

---
+++ Game/Game/Collisions/ItemCollisionHandling/MarioItemCollisionHandler.cs
@@ -0,0 +1,25 @@
+﻿using Game.Items;
+using Game.Mario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Collisions.ItemCollisionHandling
+{
+    public class MarioItemCollisionHandler
+    {
+        private MarioInstance collidingMario;
+        public IItem collidingItem;
+        public ICollisionSide collisionSide;
+
+        public MarioItemCollisionHandler(CollisionData collision) {
+            collidingMario = (MarioInstance)collision.gameObjectA;
+            collidingItem = (IItem)collision.gameObjectB;
+            collisionSide = collision.collisionSide;
+        }
+
+        
+        
+    }
+}
