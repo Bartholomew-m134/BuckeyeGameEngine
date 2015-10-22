f31c9b5906ebeb7f5e8a8089f50e8fe0540795c7
Kyle Powers <powerskyle26@yahoo.com>
2015-10-22 17:43:26 -0400

    Added capping to velocity

--- Game/Game/Utilities/ObjectPhysics.cs
+++ Game/Game/Utilities/ObjectPhysics.cs
@@ -10,17 +10,36 @@ namespace Game.Utilities
     {
         private Vector2 velocity;
         private Vector2 acceleration;
+        private Vector2 velocityMaximum = new Vector2(5, 5);
+        private Vector2 velocityMinimum = new Vector2(-5, -11);
         public ObjectPhysics()
         {
-            velocity = new Vector2(0, 0);
+            int gravity = 5;
+            velocity = new Vector2(0, gravity);
             acceleration = new Vector2(0, 0);
         }
-
+        
         public Vector2 Update(Vector2 coordinates)
         {
             Vector2 oldVelocity = velocity;
-
+            
             velocity += acceleration;
+
+            if (velocity.X > velocityMaximum.X) {
+                velocity.X = velocityMaximum.X;
+            }
+            else if (velocity.X < velocityMinimum.X) {
+                velocity.X = velocityMinimum.X;
+            }
+
+            if (velocity.Y > velocityMaximum.Y)
+            {
+                velocity.Y = velocityMaximum.Y;
+            }
+            else if (velocity.Y < velocityMinimum.Y)
+            {
+                velocity.Y = velocityMinimum.Y;
+            }
 
             return (coordinates + (oldVelocity + velocity) / 2);
            
