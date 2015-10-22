4b682e897258f6db9abcb29dc4ce427e7ebb366a
Matt Bartholomew <Bartholomew.99@osu.edu>
2015-10-22 15:17:20 -0400

    Added basis to camera

---
+++ Game/Game/Camera.cs
@@ -0,0 +1,13 @@
+﻿using Game.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+    public class Camera : ICamera
+    {
+
+    }
+}
