75a3b95b8a0383df3e16c45463063d136f5bc6d9
John Cramer <cramer.240@buckeyemail.osu.edu>
2015-10-22 15:14:04 -0400

    Layed the foundation of the physics object.

---
+++ Game/Game/Utilities/ObjectPhysics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Game.Utilities
+{
+    public class ObjectPhysics
+    {
+        private Vector2 velocity;
+        private Vector2 acceleration;
+        public ObjectPhysics()
+        {
+            velocity = new Vector2(0, 0);
+            acceleration = new Vector2(0, 0);
+        }
+
+        public Vector2 GetSetVelocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
+        }
+
+        public Vector2 GetSetAcceleration
+        {
+            get { return acceleration; }
+            set { acceleration = value; }
+        }
+
+    }
+}
