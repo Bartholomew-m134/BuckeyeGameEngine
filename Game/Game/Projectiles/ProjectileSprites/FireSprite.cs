1e1efe163e1eeda185cb75fb51fd596520a54c9a
Jackson Luken <jacksonluken@yahoo.com>
2015-10-27 18:17:50 -0400

    Created Fireball object

---
+++ Game/Game/Projectiles/ProjectileSprites/FireSprite.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Game.Interfaces;
+using System.Collections;
+
+namespace Game.Items.ItemSprites
+{
+    public class FireSprite : ISprite
+    {
+
+        private Texture2D Texture { get; set; }
+        private ArrayList fireSpriteLocations;
+        private int currentSprite;
+
+        private Vector2 currentLocation;
+
+        private Vector2 firstFireSpriteLocation;
+        private Vector2 secondFireSpriteLocation;
+        private Vector2 thirdFireSpriteLocation;
+        private Vector2 fourthFireSpriteLocation;
+
+        private Vector2 fireSpriteDimensions;
+
+        private Rectangle sourceRectangle;
+        private Rectangle destinationRectangle;
+
+        public FireSprite(Texture2D texture)
+        {
+            this.Texture = Texture;
+            currentSprite = 0;
+            fireSpriteLocations = new ArrayList();
+
+            fireSpriteDimensions.X = 8;
+            fireSpriteDimensions.Y = 8;
+
+            firstFireSpriteLocation.X = 41;
+            firstFireSpriteLocation.Y = 150;
+
+            secondFireSpriteLocation.X = 41;
+            secondFireSpriteLocation.Y = 165;
+
+            thirdFireSpriteLocation.X = 26;
+            thirdFireSpriteLocation.Y = 150;
+
+            fourthFireSpriteLocation.X = 26;
+            fourthFireSpriteLocation.Y = 165;
+
+
+
+            fireSpriteLocations.Add(thirdFireSpriteLocation);
+            fireSpriteLocations.Add(secondFireSpriteLocation);
+            fireSpriteLocations.Add(firstFireSpriteLocation);
+            fireSpriteLocations.Add(fourthFireSpriteLocation);
+        }
+
+        public void Update()
+        {
+            if (currentSprite < 3)
+            {
+                currentLocation = (Vector2)fireSpriteLocations[currentSprite];
+                currentSprite++;
+            }
+            else
+            {
+                currentSprite = 0;
+                currentLocation = (Vector2)fireSpriteLocations[currentSprite];
+                currentSprite++;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+
+            sourceRectangle = new Rectangle((int)currentLocation.X, (int)currentLocation.Y, (int)fireSpriteDimensions.X, (int)fireSpriteDimensions.Y);
+            destinationRectangle = new Rectangle((int)location.X, (int)location.Y, (int)fireSpriteDimensions.X, (int)fireSpriteDimensions.Y);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+            spriteBatch.End();
+
+        }
+
+        public Vector2 SpriteDimensions
+        {
+            get { return fireSpriteDimensions; }
+        }
+
+    }
+}
---
+++ Game/Game/Projectiles/RightFire.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Game.SpriteFactories;
+using Game.Interfaces;
+using Game.Utilities;
+
+namespace Game.Items
+{
+    public class RightFire : IProjectile
+    {
+        private Game1 myGame;
+        private ISprite FireSprite;
+        private Vector2 location;
+        private ObjectPhysics physics;
+        private bool isReleased;
+
+        public RightFire(Game1 game)
+        {
+            isReleased = false;
+            myGame = game;
+            FireSprite = ProjectileSpriteFactory.CreateFireSprite();
+            physics = new ObjectPhysics();
+            physics.Acceleration = Vector2.Zero;
+        }
+
+        public void Update()
+        {
+            FireSprite.Update();
+            location = physics.Update(location);
+        }
+
+        public void Draw(ICamera camera)
+        {
+            FireSprite.Draw(myGame.spriteBatch, camera.GetAdjustedPosition(location));
+        }
+
+        public void Explode()
+        {
+            location.Y += 2000;
+            physics.Acceleration = Vector2.Zero;
+            physics.Velocity = Vector2.Zero;
+        }
+
+        public void Release(Vector2 location)
+        {
+            if (!isReleased)
+            {
+                isReleased = true;
+                physics.ResetPhysics();
+                physics.Velocity = new Vector2(10, 10);
+                physics.Acceleration = new Vector2(0, 0);
+            }
+        }
+
+        public void ReverseDirection()
+        {
+            float Y = physics.Velocity.Y;
+            float X = physics.Velocity.X;
+            this.physics.Velocity = new Vector2(-X, Y);
+        }
+
+        public void Bounce()
+        {
+            float X = this.physics.Velocity.X;
+            this.physics.Velocity = new Vector2(X, -5);
+            this.physics.Acceleration = new Vector2(0, 1);
+        }
+
+        public Vector2 VectorCoordinates
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        public ISprite Sprite
+        {
+            get { return FireSprite; }
+            set { FireSprite = value; }
+        }
+
+        public ObjectPhysics Physics
+        {
+            get { return physics; }
+        }
+
+    }
+}
