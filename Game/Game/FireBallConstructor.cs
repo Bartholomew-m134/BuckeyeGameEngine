91903c466a21cdff500421a7c763810927dcb101
Matt Bartholomew <Bartholomew.99@osu.edu>
2015-10-27 20:00:05 -0400

    Fix For FireConstructor

--- Game/Game/FireBallConstructor.cs
+++ Game/Game/FireBallConstructor.cs
@@ -9,12 +9,19 @@ namespace Game
 {
     public class FireBallConstructor
     {
-        public static void ReleaseRightFireBall(Vector2 location, Game1 game)
+        Game1 game;
+
+        FireBallConstructor(Game1 game)
+        {
+            this.game = new Game1();
+        }
+
+        public void ReleaseRightFireBall(Vector2 location)
         {
             WorldManager.CreateNewObject(new RightFire(game));
         }
 
-        public static void ReleaseLeftFireBall(Vector2 location, Game1 game)
+        public void ReleaseLeftFireBall(Vector2 location)
         {
             WorldManager.CreateNewObject(new LeftFire(game));
         }
