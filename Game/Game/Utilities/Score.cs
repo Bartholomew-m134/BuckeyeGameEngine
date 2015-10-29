52c286bb8590626478afe827749e719dc55f8604
Connor Swick <swick.64@osu.edu>
2015-10-28 20:21:51 -0400

    more score stuff, add code readability

---
+++ Game/Game/Utilities/Score.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Utilities
+{
+    public class Score
+    {
+        public int value;
+        public int offset;
+        public Vector2 location;
+        public int timer;
+        public Score(int value, int offset, Vector2 location)
+        {
+            this.value = value;
+            this.offset = offset;
+            this.location = location;
+            timer = 10;
+        }
+        public void Update()
+        {
+            timer--;
+        }
+    }
+}
