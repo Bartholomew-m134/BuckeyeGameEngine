120b0516e754c515044d807000442ab7262ebada
John Cramer <cramer.240@buckeyemail.osu.edu>
2015-11-11 19:43:38 -0500

    Removed Constants from Physics.

---
+++ Game/Game/Utilities/Constants/ObjectPhysicsConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Utilities.Constants
+{
+    public static class ObjectPhysicsConstants
+    {
+        public const int GRAVITY = 1;
+
+        public const int INITIALXMAXVELOCITY = 6;
+        public const int INTIALYMAXVELOCITY = 12;
+
+        public const int INITIALXMINVELOCITY = -6;
+        public const int INITIALYMINVELOCITY = -12;
+
+        public const int RIGHTDAMPENTHRESHOLD = 2;
+        public const int LEFTDAMPENTHRESHOLD = -2;
+    }
+}
