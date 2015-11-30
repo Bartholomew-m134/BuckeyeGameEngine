55a5613605a192cf31233af4f4f505eb39f5a869
Connor Swick <swick.64@osu.edu>
2015-11-30 14:50:28 -0500

    add logo game state

---
+++ Game/Game/GameStates/LogoGameState.cs
@@ -0,0 +1,111 @@
+﻿using Game.Interfaces;
+using Game.SpriteFactories;
+using Game.Utilities;
+using Game.Utilities.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Game.Utilities.Constants;
+
+namespace Game.GameStates
+{
+    public class LogoGameState : IGameState
+    {
+        private Game1 game;
+        private ISprite startMenuSprite;
+        private ISprite namesLogoSprite;
+        private List<IController> controllerList;
+        private int logoCounter = 0;
+
+        public LogoGameState(Game1 game)
+        {
+            this.game = game;
+            controllerList = new List<IController>();
+            controllerList.Add(new KeyboardController(new PausedControls(game)));
+            controllerList.Add(new GamePadController(new PausedControls(game)));
+        }
+
+        public void LoadContent()
+        {
+            MenuSpriteFactory.Load(game.Content);
+            startMenuSprite = MenuSpriteFactory.CreateStartSprite();
+            namesLogoSprite = MenuSpriteFactory.CreateLogoSprite();
+        }
+
+
+        public void UnloadContent()
+        {
+
+        }
+
+        public void Update()
+        {
+            foreach (IController controller in controllerList)
+                controller.Update();
+
+            logoCounter++;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            game.GraphicsDevice.Clear(Color.Black);
+            if (logoCounter < IGameStateConstants.MENUGAMESTATELOGOCOUNTER)
+                namesLogoSprite.Draw(game.spriteBatch, Vector2.Zero);
+            else
+                startMenuSprite.Draw(game.spriteBatch, Vector2.Zero);
+        }
+
+
+        public void StartButton()
+        {
+            game.gameState = new LoadingGameState(game);
+            game.gameState.LoadContent();
+            LifeManager.Lives = IGameStateConstants.MENUGAMESTATELIVES;
+            ScoreManager.ResetScore();
+            LifeManager.ResetLives();
+            HUDManager.UpdateHUDScore(0);
+            HUDManager.ResetCoins();
+        }
+
+        public void PipeTransition(IPipe warpPipe)
+        {
+
+        }
+        public void FlagPoleTransition()
+        {
+        }
+
+
+        public void PlayerDied()
+        {
+
+        }
+
+
+        public bool IsUnderground
+        {
+            get
+            {
+                return false;
+            }
+            set
+            {
+
+            }
+        }
+
+
+        public void MarioPowerUp()
+        {
+
+        }
+
+
+        public void StateBackgroundTheme()
+        {
+        }
+    }
+}
