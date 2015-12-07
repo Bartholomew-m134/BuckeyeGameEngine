7e5360cd609b7a74dc7dbe4c303d87cea48787d9
Matt Bartholomew <Bartholomew.m134@gmail.com>
2015-12-07 15:08:54 -0500

    Added MainMenu Background

---
+++ Game/Game/Menu/MenuSprites/MainMenuBackground.cs
@@ -0,0 +1,27 @@
+﻿using Game.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Menu.MenuSprites
+{
+    public class MainMenuBackground : ISprite
+    {
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch, Microsoft.Xna.Framework.Vector2 location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Microsoft.Xna.Framework.Vector2 SpriteDimensions
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
