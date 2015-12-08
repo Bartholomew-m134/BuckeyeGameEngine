e9d9943f5b3293ad69fbffe8a4257cda32d9eb0a
Jackson Luken <jacksonluken@yahoo.com>
2015-12-07 19:00:13 -0500

    Changed elevator height to avoid clipping

--- Game/Game/Utilities/Constants/LemmingSpriteConstants.cs
+++ Game/Game/Utilities/Constants/LemmingSpriteConstants.cs
@@ -8,7 +8,7 @@ namespace Game.Utilities.Constants
 {
     public static class LemmingSpriteConstants
     {
-        public static readonly Vector2 ELEVATORDIMENSIONS = new Vector2(32, 48);
+        public static readonly Vector2 ELEVATORDIMENSIONS = new Vector2(32, 32);
         public static readonly Vector2 ELEVATORSOURCE = new Vector2(0, 0);
 
         public static readonly Vector2 ENDBLOCKDIMENSIONS = new Vector2(32, 48);
